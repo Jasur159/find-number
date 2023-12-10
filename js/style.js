8873b48881672ff26cb86ba3e1810211493ec15d
Jasur159 <ganiyevjasur7@gmail.com>
2023-12-11 00:37:18 +0500

    first

---
+++ js/style.js
@@ -0,0 +1,220 @@
+/*
+        ----------- BUttons    ---------------
+*/
+
+const uzLanguageBtn = document.getElementById("uz");
+const engLanguageBtn = document.getElementById("eng");
+const rusLanguageBtn = document.getElementById("rus");
+const resetBtn = document.getElementById("reset");
+const checkBtn = document.getElementById("Submit_btn");
+const restartBtn = document.getElementById("restart");
+const continueBtn = document.getElementById("continue");
+
+/*
+ --------------- INputs -----------------------
+*/
+const Input = document.getElementById("text");
+const referanceInput = document.querySelector(".Guessing_result");
+const scoreInput = document.getElementById("score");
+const resultBox = document.querySelector(".result");
+const opportunityInput = document.getElementById("opportunity");
+const heroText = document.querySelector(".hero");
+const scoreText = document.querySelector(".score_text");
+const restartComponent = document.querySelector(".restart");
+const restartCard = document.querySelector(".restart_card");
+const opportunityText = document.getElementById("opportunity_text");
+const tipText = document.querySelector(".tips");
+/////////////// Musics ///////////////
+const lossMusic = document.querySelector(".loss");
+const winMusic = document.querySelector(".win");
+
+let opportunityNum = 10;
+let scoreNum = 0;
+let randomNum;
+
+////////////// Create Random Number ////////////
+function createRandomNum() {
+  randomNum = Math.trunc(Math.random() * 20) + 1;
+}
+createRandomNum();
+console.log(randomNum);
+
+/////////////////////////////////////
+
+resetBtn.addEventListener("click", () => {
+  // createRandomNum();
+  // referanceInput.textContent = "Start guessing...";
+  // console.log(randomNum);
+  // Input.value = "";
+  // opportunityNum = 10;
+  // scoreNum = 0;
+  // scoreInput.textContent = `${scoreNum}`;
+  // opportunityInput.textContent = `${opportunityNum}`;
+  // resultBox.innerHTML = "?";
+  restartGame();
+});
+
+checkBtn.addEventListener("click", () => {
+  checkNum();
+});
+function checkNum() {
+  let inputValue = Number(Input.value);
+  if (!inputValue) {
+    alert("Son kiriting");
+  } else {
+    if (opportunityNum > 0) {
+      if (randomNum > inputValue) {
+        lossMusic.play();
+        opportunityNum--;
+        opportunityInput.textContent = `${opportunityNum}`;
+        if (heroText.textContent.includes("Mening")) {
+          referanceInput.textContent = "Ancha kichkina";
+        } else if (heroText.textContent.includes("Guess")) {
+          referanceInput.textContent = "Too small";
+        } else {
+          referanceInput.textContent = "Слишком маленький";
+        }
+      } else if (randomNum < inputValue) {
+        opportunityNum--;
+        lossMusic.play();
+        opportunityInput.textContent = `${opportunityNum}`;
+        if (heroText.textContent.includes("Mening")) {
+          referanceInput.textContent = "Ancha Katta";
+        } else if (heroText.textContent.includes("Guess")) {
+          referanceInput.textContent = "Too big";
+        } else {
+          referanceInput.textContent = "Слишком большой";
+        }
+      } else if (randomNum === inputValue) {
+        opportunityNum--;
+        opportunityInput.textContent = `${opportunityNum}`;
+        scoreNum++;
+        scoreInput.textContent = `${scoreNum}`;
+        resultBox.innerHTML = randomNum;
+        winMusic.play();
+        Input.blur();
+        if (heroText.textContent.includes("Mening")) {
+          referanceInput.textContent = "Siz topdingiz";
+        } else if (heroText.textContent.includes("Guess")) {
+          referanceInput.textContent = "You found it";
+        } else {
+          referanceInput.textContent = "Ты нашел это";
+        }
+        restartCard.classList.remove("hide");
+        restartComponent.classList.remove("hide");
+        return;
+      }
+    } else {
+      alert("Sizda imkoniyat qolmadi");
+    }
+  }
+  Input.value = "";
+}
+
+restartBtn.addEventListener("click", () => {
+  restartGame();
+});
+
+/////////////////// Restart function ////////////////
+function restartGame() {
+  createRandomNum();
+  if (heroText.textContent.includes("Mening")) {
+    referanceInput.textContent = "Topishni boshlang...";
+  } else if (heroText.textContent.includes("Guess")) {
+    referanceInput.textContent = "Start guessing...";
+  } else {
+    referanceInput.textContent = "Начни гадать...";
+  }
+  console.log(randomNum);
+  Input.value = "";
+  opportunityNum = 10;
+  scoreNum = 0;
+  scoreInput.textContent = `${scoreNum}`;
+  opportunityInput.textContent = `${opportunityNum}`;
+  restartComponent.classList.add("hide");
+  restartCard.classList.add("hide");
+  resultBox.innerHTML = "?";
+}
+
+//////////////// Change Languages ///////////////
+
+function changeLanguage(value) {
+  if (value === "uzb") {
+    heroText.innerHTML = "Mening raqamimni toping";
+    tipText.textContent = "(1 va 20 Orasidan)";
+    referanceInput.innerHTML = "Topishni boshlang...";
+    scoreText.innerHTML = `Natija:`;
+    opportunityText.innerHTML = `: Imkoniyat qoldi`;
+    checkBtn.textContent = "Tekshirish!";
+    Input.value = "";
+  } else if (value === "eng") {
+    tipText.textContent = "(Between 1 and 20)";
+    heroText.innerHTML = "Guess My Number!";
+    referanceInput.innerHTML = "Start guessing...";
+    scoreText.innerHTML = `Score:`;
+    opportunityText.innerHTML = `: Opportunity left`;
+    checkBtn.textContent = "Check!";
+    Input.value = "";
+  } else {
+    tipText.textContent = "(Между 1 и 20)";
+    heroText.innerHTML = "Найди мой номер!";
+    referanceInput.innerHTML = "Начни искать...";
+    scoreText.innerHTML = `Счет:`;
+    opportunityText.innerHTML = `: Возможность осталась`;
+    checkBtn.textContent = "проверять!";
+    Input.value = "";
+  }
+}
+
+uzLanguageBtn.addEventListener("click", () => {
+  changeLanguage("uzb");
+  let langValue = "uzb";
+});
+engLanguageBtn.addEventListener("click", () => {
+  changeLanguage("eng");
+  let langValue = "eng";
+});
+rusLanguageBtn.addEventListener("click", () => {
+  changeLanguage("rus");
+});
+
+continueBtn.addEventListener("click", () => {
+  if (opportunityNum > 0) {
+    createRandomNum();
+    console.log(randomNum);
+    Input.value = "";
+    restartComponent.classList.add("hide");
+    restartCard.classList.add("hide");
+    resultBox.innerHTML = "?";
+    if (heroText.textContent.includes("Mening")) {
+      referanceInput.textContent = "Topishni boshlang...";
+    } else if (heroText.textContent.includes("Guess")) {
+      referanceInput.textContent = "Start guessing...";
+    } else {
+      referanceInput.textContent = "Начни гадать...";
+    }
+  } else {
+    alert("Sizda imkoniyat qolmadi va O'yin qaytadan boshlanadi");
+    restartGame();
+  }
+});
+
+// function showReferance(langValue = "rus", inputValue) {
+//   if (randomNum > inputValue) {
+//     if (langValue === "eng") {
+//       referanceInput.textContent = "Too small";
+//     } else if (langValue === "uzb") {
+//       referanceInput.textContent = "Ancha kichkina";
+//     } else {
+//       referanceInput.textContent = "Слишком маленький";
+//     }
+//   } else if (randomNum < inputValue) {
+//     if (langValue === "eng") {
+//       referanceInput.textContent = "Too big";
+//     } else if (langValue === "uzb") {
+//       referanceInput.textContent = "Ancha katta";
+//     } else {
+//       referanceInput.textContent = "Слишком большой";
+//     }
+//   }
+// }
